a544ff3b2e8879501e969f91c7dd4a4aa03b4953
boat-atul <atul.singh@imaginemarketingindia.com>
2025-06-04 11:04:24 +0530

    to check my branch

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -49,23 +49,23 @@ const FormModal = ({
       : type === "update"
       ? "bg-teal-300"
       : "bg-transparent";
-  const [open, setOpen] = useState(false);
-  const Form = () => {
-    return type === "delete" && id ? (
-      <form action="" className="p-4 flex flex-col gap-4">
-        <span className="text-center font-medium">
-          All data will be lost. Are you sure want to delete this {table}?
-        </span>
-        <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
-          Delete
-        </button>
-      </form>
-    ) : type === "create" || type === "update" ? (
-      forms[table](type, data)
-    ) : (
-      "Form not found!"
-    );
-  };
+  const [open, setOpen] = useState(false); 
+  // const Form = () => {
+  //   return type === "delete" && id ? (
+  //     <form action="" className="p-4 flex flex-col gap-4">
+  //       <span className="text-center font-medium">
+  //         All data will be lost. Are you sure want to delete this {table}?
+  //       </span>
+  //       <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
+  //         Delete
+  //       </button>
+  //     </form>
+  //   ) : type === "create" || type === "update" ? (
+  //     forms[table](type, data)
+  //   ) : (
+  //     "Form not found!"
+  //   );
+  // };
   return (
     <>
       <button
